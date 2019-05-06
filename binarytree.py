319916bfcde4aadfb3184d4c5795fb3ff16d9b2a
timo <tmakhlay2@gmail.com>
2019-05-06 15:31:10 -0700

    Small update

--- binarytree.py
+++ binarytree.py
@@ -237,11 +237,11 @@ class BinarySearchTree(object):
         TODO: Running time: ??? Why and under what conditions?
         TODO: Memory usage: ??? Why and under what conditions?"""
         # TODO: Traverse left subtree, if it exists
-        ...
+
         # TODO: Visit this node's data with given function
-        ...
+
         # TODO: Traverse right subtree, if it exists
-        ...
+        
 
     def _traverse_in_order_iterative(self, node, visit):
         """Traverse this binary tree with iterative in-order traversal (DFS).
