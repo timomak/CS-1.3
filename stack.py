1189c0fea0f4dfefa0c10d639d51a09628e8d991
timo <tmakhlay2@gmail.com>
2019-05-06 13:29:03 -0700

    Working LinkedStack

--- stack.py
+++ stack.py
@@ -1,7 +1,7 @@
+# coding: utf-8
 #!python
 
 from linkedlist import LinkedList
-
 
 # Implement LinkedStack below, then change the assignment at the bottom
 # to use this Stack implementation to verify it passes all tests
@@ -22,27 +22,40 @@ class LinkedStack(object):
     def is_empty(self):
         """Return True if this stack is empty, or False otherwise."""
         # TODO: Check if empty
+        return self.list.is_empty()
 
     def length(self):
         """Return the number of items in this stack."""
         # TODO: Count number of items
+        return self.list.length()
 
     def push(self, item):
         """Insert the given item on the top of this stack.
         Running time: O(???) – Why? [TODO]"""
         # TODO: Push given item
+        self.list.prepend(item)
 
     def peek(self):
         """Return the item on the top of this stack without removing it,
         or None if this stack is empty."""
         # TODO: Return top item, if any
+        temp = self.list.head
+        if temp != None:
+            return temp.data
+        else:
+            return None
 
     def pop(self):
         """Remove and return the item on the top of this stack,
         or raise ValueError if this stack is empty.
         Running time: O(???) – Why? [TODO]"""
         # TODO: Remove and return top item, if any
-
+        temp = self.list.head
+        if temp != None:
+            self.list.delete(temp.data) # O(n)
+            return temp.data
+        else:
+            raise ValueError("Empty list")
 
 # Implement ArrayStack below, then change the assignment at the bottom
 # to use this Stack implementation to verify it passes all tests
@@ -89,3 +102,9 @@ class ArrayStack(object):
 # to use each of your Stack implementations to verify they each pass all tests
 Stack = LinkedStack
 # Stack = ArrayStack
+# if __name__ == '__main__':
+#     Stack = LinkedStack()
+#     # Stack = ArrayStack()
+#
+#     # Stack.push(item="C")
+#     print(Stack.peek())
