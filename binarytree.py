e24e281229f6066fe15b382ea969928d6ab74fc7
timo <tmakhlay2@gmail.com>
2019-05-19 17:28:17 -0700

    Fixed binary search.

--- binarytree.py
+++ binarytree.py
@@ -1,5 +1,6 @@
 #!python
 
+from queue import LinkedQueue
 
 class BinaryTreeNode(object):
 
@@ -348,20 +349,24 @@ class BinarySearchTree(object):
         Start at the given node and visit each node with the given function.
         TODO: Running time: ??? Why and under what conditions?
         TODO: Memory usage: ??? Why and under what conditions?"""
-        # TODO: Create queue to store nodes not yet traversed in level-order
-        # queue = ...
-        # # TODO: Enqueue given starting node
-        # ...
-        # # TODO: Loop until queue is empty
-        # while ...:
-        #     # TODO: Dequeue node at front of queue
-        #     node = ...
-        #     # TODO: Visit this node's data with given function
-        #     ...
-        #     # TODO: Enqueue this node's left child, if it exists
-        #     ...
-        #     # TODO: Enqueue this node's right child, if it exists
-        #     ...
+
+        # Thank you Connor and Lucia for great references: https://github.com/lvreynoso/CS1.3-Coursework/blob/master/binarytree.py
+        # Create queue to store nodes not yet traversed in level-order
+        queue = LinkedQueue()
+        # Enqueue given starting node
+        queue.enqueue(start_node)
+        # Loop until queue is empty
+        while not queue.is_empty():
+            # Dequeue node at front of queue
+            node = queue.dequeue()
+            # Visit this node's data with given function
+            visit(node.data)
+            # Enqueue this node's left child, if it exists
+            if node.left is not None:
+                queue.enqueue(node.left)
+            # Enqueue this node's right child, if it exists
+            if node.right is not None:
+                queue.enqueue(node.right)
 
 
 def test_binary_search_tree():
